74949e1145f205b73e8d60c907ac8d02b8fccb3b
Michael Zhan <michaelzhan2001@gmail.com>
2023-12-11 12:49:04 -0500

    Reorganize code

--- newton/newton.py
+++ newton/newton.py
@@ -3,20 +3,14 @@ import numdifftools as nd
 from deap import benchmarks
 
 
-def f(x):
-    """Function to optimize."""
-    return benchmarks.bohachevsky(x)[0]
-
-def gradient(x):
-    """Gradient of f."""
-    return nd.Gradient(f)(x)
-
-def hessian(x):
-    """Hessian of f."""
-    return nd.Hessian(f)(x)
-
-def newton(x0, tol=1e-10, maxiter=1000):
+def newton(x0, f, tol=1e-10, maxiter=1000):
     """Newton's method."""
+    def gradient(x):
+        return nd.Gradient(f)(x)
+
+    def hessian(x):
+        return nd.Hessian(f)(x)
+
     x = x0
     prev = x0
     for i in range(maxiter):
@@ -29,8 +23,8 @@ def newton(x0, tol=1e-10, maxiter=1000):
     return x
 
 def main():
-    x0 = np.array([-0.001, 0.001])
-    x = newton(x0)
+    x0 = np.array([1e-9, 1e-9])
+    x = newton(x0, lambda x: benchmarks.ackley(x)[0])
     print(x)
 
 
