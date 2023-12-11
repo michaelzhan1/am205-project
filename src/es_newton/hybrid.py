947dbe941dcbf7bf6d2cc10c947d166619a2f1f1
Michael Zhan <michaelzhan2001@gmail.com>
2023-12-11 14:36:07 -0500

    Fix import errors due to folder name

---
+++ src/es_newton/hybrid.py
@@ -0,0 +1,11 @@
+from newton.newton import newton
+from typing import Callable
+from es.es import evo_strat
+import numpy as np
+from deap import benchmarks
+
+
+def es_newton(f: Callable, n: int, children: int=1000, parents: int=100, x0=None, tol=1e-8, display=True, max_iter:int = 100):
+    x_es = evo_strat(f, n, children, parents, x0, tol, display, max_iter)
+    init_newt = x_es.x
+    return newton(f, init_newt, 1e-12, max_iter)
