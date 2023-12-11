863e229c9c5991e8bca35a6710e06c319168f7ae
Michael Zhan <michaelzhan2001@gmail.com>
2023-12-11 10:20:00 -0500

    Update stopping condition for x values

--- newton.py
+++ newton.py
@@ -5,7 +5,7 @@ from deap import benchmarks
 
 def f(x):
     """Function to optimize."""
-    return benchmarks.ackley(x)[0]
+    return benchmarks.bohachevsky(x)[0]
 
 def gradient(x):
     """Gradient of f."""
@@ -18,16 +18,18 @@ def hessian(x):
 def newton(x0, tol=1e-10, maxiter=1000):
     """Newton's method."""
     x = x0
+    prev = x0
     for i in range(maxiter):
         x = x - np.linalg.pinv(hessian(x)) @ gradient(x)
-        if np.linalg.norm(gradient(x)) < tol:
+        if np.linalg.norm(x - prev) < tol:
             break
+        prev = x
     else:
         print(f'Failed to reach tolerance {tol} in {maxiter} iterations.')
     return x
 
 def main():
-    x0 = np.array([1.2, 1.2])
+    x0 = np.array([-0.001, 0.001])
     x = newton(x0)
     print(x)
 
