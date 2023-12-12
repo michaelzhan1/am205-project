d6a110b99bcce1abff63fffe9dbfd50373cad7bf
Michael Zhan <michaelzhan2001@gmail.com>
2023-12-11 21:18:30 -0500

    Set tolerance to 1e-15 for newton

--- src/cmaes_newton/hybrid.py
+++ src/cmaes_newton/hybrid.py
@@ -7,5 +7,5 @@ from deap import benchmarks
 def cmaes_newton(f: Callable, n: int, children: int=1000, parents: int=100, x0=None, tol=1e-12, display=True, max_iter:int = 100, name='undefined'):
     x_cmaes, cmaes_count = cma_evo_strat(f, n, children, parents, x0, tol, display, max_iter)
     init_newt = x_cmaes.x
-    x_newton, newton_count = newton(f, init_newt, 1e-14, max_iter)
+    x_newton, newton_count = newton(f, init_newt, 1e-15, max_iter)
     return x_newton, newton_count + cmaes_count
