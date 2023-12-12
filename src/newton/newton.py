2acdc9589a7acb245f1fdf04411a37608aef9dcf
Michael Zhan <michaelzhan2001@gmail.com>
2023-12-11 20:46:32 -0500

    Add newton plotting

--- src/newton/newton.py
+++ src/newton/newton.py
@@ -1,20 +1,25 @@
 import numpy as np
 import numdifftools as nd
 from deap import benchmarks
+from newton.plot import initialize_plot, plot_points
 
 
-def newton(f, x0, tol=1e-10, maxiter=1000, name='undefined'):
+def newton(f, x0, tol=1e-10, maxiter=1000, name='undefined', plot=False):
     """Newton's method."""
     def gradient(x):
         return nd.Gradient(f)(x)
 
     def hessian(x):
         return nd.Hessian(f)(x)
+    
+    if plot:
+        initialize_plot(f)
 
     count = 0
     x = x0
     prev = x0
     for i in range(maxiter):
+        plot_points(f, x, f'{str(i).zfill(2)}_newton.png')
         count += 1
         x = x - np.linalg.pinv(hessian(x)) @ gradient(x)
         if np.linalg.norm(x - prev) < tol:
