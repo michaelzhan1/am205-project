1733b7463d506644afe49340cf7479647e62491d
Michael Zhan <michaelzhan2001@gmail.com>
2023-12-11 20:11:54 -0500

    Format output table for run.py

--- src/run.py
+++ src/run.py
@@ -6,6 +6,7 @@ from cmaes_newton.hybrid import cmaes_newton
 import numpy as np
 import scipy
 from deap import benchmarks
+from tabulate import tabulate
 
 
 np.random.seed(1)
@@ -26,14 +27,13 @@ def main():
     print('CMA-ES-Newton done')
     x_scipy = scipy.optimize.minimize(f, x0, method='BFGS', tol=1e-12)
     print('Scipy done')
-    print('-------------------------')
+    print('-----------------------------------------------------------------------------')
 
-    print(f"Newton's method results: {x_newton} after {count_newton} iterations")
-    print(f"Evolutionary strategy results: {x_es.x} after {count_es} iterations")
-    print(f"Newton-ES hybrid results: {x_es_newton} after {count_es_newton} iterations")
-    print(f"CMA-ES results: {x_cmaes.x} after {count_cmaes} iterations")
-    print(f"CMA-ES-Newton hybrid results: {x_cmaes_newton} after {count_cmaes_newton} iterations")
-    print(f"Scipy results: {x_scipy.x} after {x_scipy.nit} iterations")
+    method_names = ['Newton', 'ES', 'ES-Newton', 'CMA-ES', 'CMA-ES-Newton', 'Scipy']
+    x_values = list(map(list, [x_newton, x_es.x, x_es_newton, x_cmaes.x, x_cmaes_newton, x_scipy.x]))
+    count_values = [count_newton, count_es, count_es_newton, count_cmaes, count_cmaes_newton, x_scipy.nit]
+    headers = ['Method', 'x*', 'Iterations']
+    print(tabulate(list(zip(method_names, x_values, count_values)), headers=headers, tablefmt='orgtbl'))
     
 
 if __name__ == "__main__":
